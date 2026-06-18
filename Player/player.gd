6ed797ba51b5fe3153d24c714584f7a372574167
uj404 <39839409+uj404@users.noreply.github.com>
2026-06-18 23:52:19 +0530

    damage overlay & game over menu

--- Player/player.gd
+++ Player/player.gd
@@ -7,13 +7,19 @@ const SPEED = 5.0
 @export var max_hitpoints: int = 100
 
 @onready var camera_pivot: Node3D = $CameraPivot
+@onready var damage_animation_player: AnimationPlayer = $DamageTexture/DamageAnimationPlayer
+@onready var game_over_menu: Control = $GameOverMenu
+
 var mouse_motion := Vector2.ZERO
 var hitpoints : int = max_hitpoints:
 	set(value):
+		if value < hitpoints:
+			damage_animation_player.stop(false)
+			damage_animation_player.play("TakeDamage")
 		hitpoints = value
 		print(hitpoints)
 		if hitpoints <= 0 :
-			get_tree().quit()
+			game_over_menu.game_over()
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
