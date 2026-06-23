cbe5c4234ffec190ef43f6cca6eca0c79f0d8852
uj404 <39839409+uj404@users.noreply.github.com>
2026-06-23 18:06:22 +0530

    building level, replacing enemy with model and animating

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,14 +2,14 @@ extends CharacterBody3D
 class_name Enemy
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
 @export var max_hitpoints := 100
 @export var attack_range := 1.5
 @export var attack_damage := 20
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
 
 var player
 var provoked := false
@@ -41,11 +41,10 @@ func _physics_process(delta: float) -> void:
 
 	if distance <= aggro_range:
 		provoked = true
+	
+	if provoked:
 		if distance <= attack_range:
-			animation_player.play("Attack");
-
-	else:
-		provoked = false 
+			playback.travel("Attack")
 
 	if direction:
 		look_at_target(direction)
